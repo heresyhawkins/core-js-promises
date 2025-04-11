8daacc871948e06e4a78ac0002a3cda8b89103af
heresyhawkins <heresyhawkins@gmail.com>
2025-04-12 01:56:07 +0300

    complete task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -112,8 +112,12 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((results) =>
+    results.map((result) =>
+      result.status === 'fulfilled' ? result.value : null
+    )
+  );
 }
 
 /**
@@ -134,8 +138,12 @@ function getAllResult(/* promises */) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce((chain, promise) => {
+    return chain.then((result) => {
+      return promise.then((value) => result + value);
+    });
+  }, Promise.resolve(''));
 }
 
 module.exports = {
